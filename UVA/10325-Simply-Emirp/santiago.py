a0829ea060bca29aa1efe8e8ce45acf995f32f50
Ricardo Alexis Quiroz Bazan <saquirozbazan@gmail.com>
2016-02-26 18:19:46 -0500

    Santiago adds solution for 10325 - UVA

---
+++ UVA/10325-Simply-Emirp/santiago.py
@@ -0,0 +1,26 @@
+# UVA ya acepta Python3 
+
+def primes_upto(limit):
+    is_prime = [False, True] + [True] * (limit - 1) 
+    for n in range(2, int(limit**0.5 + 1.5)): # stop at ``sqrt(limit)``
+        if is_prime[n]:
+            for i in range(n*n, limit+1, n):
+                is_prime[i] = False
+    return is_prime
+
+def main():
+    try:
+        is_prime = primes_upto(1000000)
+        while True:
+            s = input()
+            a, b = int(s), int(s[::-1])
+            if not is_prime[a]:
+                print("{} is not prime.".format(s))
+            elif (is_prime[a] and a == b) or not (is_prime[a] and is_prime[b]):
+                print("{} is prime.".format(s))
+            else:
+                print("{} is emirp.".format(s))
+    except:
+        pass
+    
+main()
